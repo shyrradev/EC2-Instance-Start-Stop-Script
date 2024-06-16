79be3e89d5b90ffad06aff085f2a06128d32a630
shyrradev <shayanobrohi@gmail.com>
2024-06-16 14:10:17 +0500

    Updated the ss-script.sh script: Fault Tolerance and Work

--- ss-script.sh
+++ ss-script.sh
@@ -6,20 +6,32 @@ INSTANCE_ID="enter instance id"
 
 #start/stop ec2 instance based on user input
 
-if [ $1 == "start" ]; then
-    aws ec2 start-instances --instance-ids $INSTANCE_ID
-    echo "instance started"
-elif [ $1 == "stop" ]; then
-    aws ec2 stop-instances --instance-ids $INSTANCE_ID
-    echo "instance stopped"
+if [ -z "$1" ]; then
+    echo "invalid command. Usage: ./ss-script.sh start/stop or NO INSTANCE-ID UPLOADED"
+    exit 1
+fi
+
+if [[ "$1" == "start" ]]; then
+    aws ec2 start-instances --instance-ids "$INSTANCE_ID"
+    if [ $? -ne 0 ]; then
+        echo "Failed to start instance. Please check the instance ID and try again."
+        exit 1
+    else
+        echo "Instance started successfully."
+    fi
+elif [[ "$1" == "stop" ]]; then
+    aws ec2 stop-instances --instance-ids "$INSTANCE_ID"
+    if [ $? -ne 0 ]; then
+        echo "Failed to stop instance. Please check the instance ID and try again."
+        exit 1
+    else
+        echo "Instance stopped successfully."
+    fi
 else
     echo "invalid command. Usage: ./ss-script.sh start/stop"
+    exit 1
 fi
+
 
 #check if command executed successfully
 
-if [ $? -eq 0 ]; then
-    echo "$INSTANCE_ID EXECUTED SUCCESSFULLY"
-    else
-    echo "EXECUTION FAILED"
-fi
